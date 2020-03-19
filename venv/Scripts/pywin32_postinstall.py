9ba4030f5c46f2285d3e35ff3919f75c308477c6
Ian Ward <golfinian@gmail.com>
2020-03-18 21:21:42 -0700

    actually remove venv

--- venv/Scripts/pywin32_postinstall.py
+++
@@ -1,4 +0,0 @@
-#!c:\users\ianwa\onedrive\documents\github\distributed-fileshare-app\venv\scripts\python.exe
-# EASY-INSTALL-SCRIPT: 'pywin32==227','pywin32_postinstall.py'
-__requires__ = 'pywin32==227'
-__import__('pkg_resources').run_script('pywin32==227', 'pywin32_postinstall.py')
