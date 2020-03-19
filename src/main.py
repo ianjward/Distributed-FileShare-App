8a9ee11c125510b034bfc2089d07195802c65a81
Ian Ward <golfinian@gmail.com>
2020-03-19 15:16:24 -0700

    attempting udp

--- src/main.py
+++ src/main.py
@@ -1,29 +1,23 @@
-import socket
 from src.utilities.files import ShareFile, monitor_file_changes
 from src.peer_types.master_peer import MasterNode
 from src.peer_types.slave_peer import SlaveNode
 from twisted.internet import reactor
-
-
-# Returns internet facing IP. Might not work without internet? But works on both linux and Windows while others did not.
-def get_local_ip_address():
-    internet = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    internet.connect(("8.8.8.8", 80))
-    return internet.getsockname()[0]
+import src.utilities.networking as network
 
 
 if __name__ == '__main__':
     server_port = 3025
 
-    MasterNode(server_port, "MyTestShare", '1234')
-    share_slave = SlaveNode(3025, get_local_ip_address())
+    # MasterNode(server_port, "MyTestShare", '1234')
+    # share_slave = SlaveNode(3025, '192.168.1.105')
 
 
     # @TODO Need to wait until shares have been found before this is run
-    # share_ips = local_network.available_shares.split(" ")
-
+    master_ips_string = network.find_lan_shares()
+    master_ips = master_ips_string.split(" ")
+    print(master_ips)
     # Make this computer act as the master for a share
-    # MasterNode(server_port, "MyTestShare", '1234')
+    MasterNode(server_port, "MyTestShare", '1234')
 
     # Make this computer act as a slave for a share (testing purposes)
     # share_slave = SlaveNode(3025, network.get_local_ip_address())
--- src/utilities/messages.py
+++ src/utilities/messages.py
@@ -2,10 +2,18 @@ import pickle
 import socket
 
 
+# Returns internet facing IP. Might not work without internet? But works on both linux and Windows while others did not.
+def get_local_ip_address():
+    internet = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    internet.connect(("8.8.8.8", 80))
+    return internet.getsockname()[0]
+
+
 class Message:
     def __init__(self, msg_type: str):
         super().__init__()
         self.mType = msg_type
+        self.sender_ip = get_local_ip_address()
 
     def encode_msg(self):
         return pickle.dumps(self)
