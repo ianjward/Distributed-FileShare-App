353958c0f403cb61885108d5549300f30a22df93
ianjward <golfinian@gmail.com>
2020-03-20 12:17:50 -0700

    mashed everything together: hideous but might work

--- src/main.py
+++ src/main.py
@@ -1,27 +1,10 @@
-from src.utilities.files import ShareFile, monitor_file_changes
-from src.peer_types.master_peer import MasterNode
-from src.peer_types.slave_peer import SlaveNode
 from twisted.internet import reactor
-import src.utilities.networking as network
+import src.utilities.protocols as network
 
 
 if __name__ == '__main__':
-    server_port = 3025
-
-    # MasterNode(server_port, "MyTestShare", '1234')
     # share_slave = SlaveNode(3025, '192.168.1.105')
-
-
-    # @TODO Need to wait until shares have been found before this is run
-    master_ips_string = network.find_lan_shares()
-    master_ips = master_ips_string.split(" ")
-    print(master_ips)
-    # Make this computer act as the master for a share
-    MasterNode(server_port, "MyTestShare", '1234')
-
-    # Make this computer act as a slave for a share (testing purposes)
-    # share_slave = SlaveNode(3025, network.get_local_ip_address())
-    #
+    network.find_lan_shares()
     # share_file = ShareFile('monitored_files/test.txt')
     # share_file.__hash__()
     # monitor_file_changes(share_slave)
--- src/utilities/networking.py
+++ src/utilities/networking.py
@@ -1,44 +1,7 @@
 import socket
-import time
 from twisted.application import internet
 from src.utilities.protocols import NetworkDiscoveryProtocol
 
 
-# # Discovers all available shares on the lan
-def find_lan_shares() -> str:
-    print("Searching for Lan Shares")
-
-    discovery_protocol = NetworkDiscoveryProtocol()
-    server = internet.UDPServer(7999, discovery_protocol)
-    server.startService()
-    time.sleep(6)
-    return discovery_protocol.available_shares
 
 
-# Returns internet facing IP. Might not work without internet? But works on both linux and Windows while others did not.
-def get_local_ip_address():
-    internet = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    internet.connect(("8.8.8.8", 80))
-    return internet.getsockname()[0]
-
-
-# def getDummyData(x):
-#     """
-#     This function is a dummy which simulates a delayed result and
-#     returns a Deferred which will fire with that result. Don't try too
-#     hard to understand this.
-#     """
-#     d = defer.Deferred()
-#     # simulate a delayed result by asking the reactor to fire the
-#     # Deferred in 2 seconds time with the result x * 3
-#     reactor.callLater(2, d.callback, x * 3)
-#     return d
-#
-# def printData(d):
-#     """
-#     Data handling function to be added as a callback: handles the
-#     data by printing the result
-#     """
-#     print d
-#
-# d = getDummyData(3)
